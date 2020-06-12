5d9e1309c21a37e026fb626e29087b6a69d2d4e8
ACheong97 <32020345+ACheong97@users.noreply.github.com>
2020-06-12 00:31:31 -0700

    Update datalogger.py

--- datalogger.py
+++ datalogger.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun  8 09:32:02 2020
+Created on Tue Jun  9 22:05:57 2020
 
 @author: Aaron
 """
@@ -13,42 +13,61 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use("tkAgg")
 import numpy as np
+import tkinter
 
+#serial communication
 ser = serial.Serial('COM5')
 ser.flushInput() #clear queue to avoid data overlap
 
-plot_window = 100
+#plotting settings
+plot_window = 100 #plot window width
 y_var = np.array(np.zeros([plot_window]))
 
-plt.ion()
-fig, ax = plt.subplots()
-line, = ax.plot(y_var)
+plt.ion() #interactive mode on
+#fig, ax = plt.subplots()
+#line, = ax.plot(y_var)
 
+#1st button
+def log_data():
+    plot_window = 100 #plot window width
+    y_var = np.array(np.zeros([plot_window]))
 
-while True:
-    try:
-        ser_bytes = ser.readline()
-        
+    plt.ion() #interactive mode on
+    fig, ax = plt.subplots()
+    line, = ax.plot(y_var)
+    
+    while True:
         try:
-            decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-            print(decoded_bytes)
-        except:
-            continue
-        
-        #data write
-        with open("test_data.csv","a") as f:
-            writer = csv.writer(f,delimiter=",")
-            writer.writerow([time.time(),decoded_bytes])
+            ser_bytes = ser.readline()
             
-        y_var = np.append(y_var,decoded_bytes)
-        y_var = y_var[1:plot_window+1]
-        line.set_ydata(y_var)
-        ax.relim()
-        ax.autoscale_view()
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-        
-    except KeyboardInterrupt: #ctrl+c to stop
-        print('interrupted!')
-        break
+            try:
+                decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+                print(decoded_bytes)
+            except:
+                continue
+            
+            #data write
+            with open("test_data.csv","a") as f:
+                writer = csv.writer(f,delimiter=",")
+                writer.writerow([time.time(),decoded_bytes])
+                
+            y_var = np.append(y_var,decoded_bytes)
+            y_var = y_var[1:plot_window+1]
+            line.set_ydata(y_var)
+            ax.relim()
+            ax.autoscale_view()
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+            
+        except KeyboardInterrupt: #ctrl+c to stop
+            print('interrupted!')
+            break
 
+#Widgets
+m = tkinter.Tk() #Main Window
+
+#Data Logger Button Widget
+startlog = tkinter.Button(m, text = 'Start logging data', command = log_data)
+startlog.grid()
+
+m.mainloop() 
