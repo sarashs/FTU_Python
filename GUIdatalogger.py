692fb75fc9c47573421d25f3570bdcd1ef498c9b
ACheong97 <32020345+ACheong97@users.noreply.github.com>
2020-06-15 08:56:38 -0700

    Update and rename datalogger.py to GUIdatalogger.py

--- GUIdatalogger.py
+++ GUIdatalogger.py
@@ -13,22 +13,21 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use("tkAgg")
 import numpy as np
-import tkinter
+from tkinter import *
 
 #serial communication
-ser = serial.Serial('COM5')
+ser = serial.Serial('COM5',baudrate=9600)
+ser.write(b'yo')
 ser.flushInput() #clear queue to avoid data overlap
 
 #plotting settings
-plot_window = 100 #plot window width
+plot_window = 500 #plot window width
 y_var = np.array(np.zeros([plot_window]))
 
 plt.ion() #interactive mode on
-#fig, ax = plt.subplots()
-#line, = ax.plot(y_var)
 
 #1st button
-def log_data():
+def log_data(value):
     plot_window = 100 #plot window width
     y_var = np.array(np.zeros([plot_window]))
 
@@ -36,10 +35,15 @@ def log_data():
     fig, ax = plt.subplots()
     line, = ax.plot(y_var)
     
+    if(value==1):
+        ax.set_yscale('linear')
+    else:
+        ax.set_yscale('log')
+    
     while True:
         try:
             ser_bytes = ser.readline()
-            
+            #time.sleep(sample_rate)
             try:
                 decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
                 print(decoded_bytes)
@@ -64,10 +68,36 @@ def log_data():
             break
 
 #Widgets
-m = tkinter.Tk() #Main Window
+master = Tk() #Main Window
+master.minsize(300,300) #dimensions
+
+#entry for collection rate
+e = Entry(master)
+e.grid()
+sample_rate = e.get()
+
+#selects radiobutton
+def func1():
+    if v.get() == 1:
+        log_data(1)
+        return 0
+    else:
+        log_data(2)
+        return 1
 
 #Data Logger Button Widget
-startlog = tkinter.Button(m, text = 'Start logging data', command = log_data)
+#Log
+v = IntVar()
+v.set(1)
+
+a = Radiobutton(master, text = 'Linear Plot', value = 1, variable = v)
+a.grid()
+b = Radiobutton(master, text = 'Logarithmic Plot', value = 2, variable = v)
+b.grid()
+
+button_sel = v.get() #get current value of v
+
+startlog = Button(master, text = 'Start logging data', command = func1)
 startlog.grid()
 
-m.mainloop() 
+master.mainloop() 
