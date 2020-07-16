e317f6da3fc8fa39b0ce7604ab9d01db9dd93f47
Valentine Ssebuyungo <v.ssebuyungo@alumni.ubc.ca>
2020-07-16 16:53:42 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,164 +1,146 @@
-/*
- * MCU_code.c
- *
- * Created: 7/8/2020 12:21:33 PM
- * Author : hp
- */ 
+#include <atmel_start.h>
+#include <tcc_lite.h>
+#include <hri_tcc_d21.h>
 
-
-#include "sam.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
 
+/************************************************************************/
+/* PINS DESCRIPTIONS                                                    */
+/************************************************************************/
 
-#define LED1 PORT_PB30;
-#define LED0 PORT_PA06;
-#define TIMER4_FREQUENCY 1000000; //1MHz
-#define TIMER4_PRESCALAR 256;
+/*
+#define TCC1_out GPIO(GPIO_PORTA, 3)
+#define HEATER_PWM GPIO(GPIO_PORTA, 4)
+#define HELMHOLTZ_PWM GPIO(GPIO_PORTA, 5)
+#define Q11_FREQ_COUNTER GPIO(GPIO_PORTA, 8)
+#define Q12_FREQ_COUNTER GPIO(GPIO_PORTA, 9)
+#define START_ADC GPIO(GPIO_PORTA, 10)
+#define _DRDY_ADC GPIO(GPIO_PORTA, 11)
+#define MOSI_ADC GPIO(GPIO_PORTA, 16)
+#define SCK_ADC GPIO(GPIO_PORTA, 17)
+#define MISO_ADC GPIO(GPIO_PORTA, 19)
+#define _CS_ADC GPIO(GPIO_PORTA, 21)
+#define _RESET_ADC GPIO(GPIO_PORTA, 22)
+#define _PWDN_ADC GPIO(GPIO_PORTA, 23)
+#define RED_LED GPIO(GPIO_PORTB, 10)
+#define BLUE_LED GPIO(GPIO_PORTB, 11)
+#define Q14_FREQ_COUNTER GPIO(GPIO_PORTB, 22)
+#define Q13_FREQ_COUNTER GPIO(GPIO_PORTB, 23)
+*/
 
-void init_TC4(void);
-void init_TC5(void);
-void clock_setup(void);
+
+//hri_tcc_write_CC_reg(TCC1, 0, 0xf41); /* Channel 0 Compare/Capture Value: 0xf41 */ //how to change serial rate
+#define TCC1_FREQUENCY_VAL 1000000 //1MHz
+#define TCC1_PRESCALAR_VAL 256
+
 float volt_to_temperature(float);
 int twos_complement_to_int(int, int);
 int ADC_mv(int, int);
 int magnetic_field_mT(int);
+//system FSM
 int transition_system_FSM(bool, bool, int);
-unsigned int test_time_left(clock_t,int);
-//
 void run_system_FSM(int);
 void ADC_array_convert(void);
+//magnetic FSM
+void magnetic_fsm_run(void);
+void magnetic_fsm_transition(void);
+
 
 //volatile is for variables that can always change in the code
-volatile int timer4_compare_value = 3905;  //compare value for 1sec default rate 
+volatile int TCC1_compare_value = 3905;  //compare value for 1sec default rate
+
 volatile int raw_ADC_data [29];       //stores raw ADC values
 volatile int converted_ADC_data [29]; //stores converted ADC values
-volatile int test_time_count;
+volatile int ADC_GAIN = 1;
+
+volatile int test_time_count = 0;     //seconds of test time elapsed
 volatile bool TEST_START = false;
 volatile bool TEST_STOP = true;
 
-//Input data
+//Input data from the MCU
+volatile int desired_temperature = 0;     //in C
+volatile int desired_magnetic_field = 0;  //in mT
+volatile int desired_time_for_test = 0;	 //time in minutes
+volatile int desired_FPGA_voltage =0;     //in mV
+volatile int serial_output_rate=1000;                 //rate to read data in ms
+
+//Variables for Magnetic field FSM
+volatile int measured_magnetic_field = 0; // in mT
+volatile int magnetic_PWM_duty = 0; //duty cycle value 0-255
+volatile char magnetic_fsm_state = 0; //idle state
+
 
 
 int main(void)
 {
+	/* Initializes MCU, drivers and middle ware */
+	atmel_start_init();
+	
+	
+	/* Configure/Enable Interrupts */
+	NVIC_SetPriority(TCC1_IRQn, 2);											
+	NVIC_EnableIRQ(TCC1_IRQn);
+	
+	NVIC_SetPriority(TCC2_IRQn, 1);
+	NVIC_EnableIRQ(TCC2_IRQn);
+	
+	NVIC_SetPriority(WDT_IRQn, 0);
+	NVIC_EnableIRQ(WDT_IRQn);													
+	
+	/* Enable all IRQs */
+	__enable_irq();
+
 	/* Replace with your application code */
-	SystemInit(); //Initialize system
-	clock_setup(); //Sets up the clocks
-	init_TC4(); // Enables TC4 timer with the predefined interrupts
-	
-	//Configure LED0 and LED 1 as OUTPUTS
-	REG_PORT_DIRSET1 = LED0;
-	
-	while (1)
+	while (1) {
+	}
+}
+
+void TCC1_Handler(void){
+	//serial rate CC0
+	//write code to send serial data
+	if ( hri_tcc_get_interrupt_MC0_bit(TCC1) )
 	{
+		//1. Increase test time by 1s
+		test_time_count++;
+		
+		//2. Clear interrupt bit
+		hri_tcc_clear_interrupt_MC0_bit(TCC1);
 		
 	}
-	return 0;
-}
-void clock_setup(){ //setting up the clock speeds
-	// Set up the generic clock (GCLK4) used to clock timers
-	REG_GCLK_GENDIV = GCLK_GENDIV_DIV(48) |          // Divide the 48MHz clock source by divisor 3: 48MHz/3=16MHz
-	GCLK_GENDIV_ID(4);            // Select Generic Clock (GCLK) 4
-	while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
-
-	REG_GCLK_GENCTRL = GCLK_GENCTRL_IDC |           // Set the duty cycle to 50/50 HIGH/LOW
-	GCLK_GENCTRL_GENEN |         // Enable GCLK4
-	GCLK_GENCTRL_SRC_DFLL48M |   // Set the 48MHz clock source
-	GCLK_GENCTRL_ID(4);          // Select GCLK4
-	while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
 	
 }
 
-void init_TC4(){ //initialize TC4 timer
-	// Feed GCLK4 to TC4 and TC5
-	REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |         // Enable GCLK4 to TC4 and TC5
-	GCLK_CLKCTRL_GEN_GCLK4 |     // Select GCLK4
-	GCLK_CLKCTRL_ID_TC4_TC5;     // Feed the GCLK4 to TC4 and TC5
-	//GCLK_CLKCTRL_ID_TCC2_TC3; this can feed it to TC3 as well
-	while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
-	
-	// Configure TC4 (16 bit counter by default)
-	
-	
-	REG_TC4_COUNT16_CC0 = 0x1;                      // Set the TC4 CC0 register to  , decimal 1952(0x7A0) -> 0.5 sec
-	                                                // This is the rate of reading the ADC
-	while (TC4->COUNT16.STATUS.bit.SYNCBUSY);        // Wait for synchronization
-	REG_TC4_COUNT16_CC1 = 0xF41;                      // Set the TC4 CC1 register to  decimal 3905, 1 sec
-													  // This is the rate at which serial data is sent 
-	while (TC4->COUNT16.STATUS.bit.SYNCBUSY);        // Wait for synchronization
-	
-	//Enabling interrupts
-	NVIC_SetPriority(TC4_IRQn, 1);    // Set the Nested Vector Interrupt Controller (NVIC) priority for TC4 to 0 (highest)
-	NVIC_EnableIRQ(TC4_IRQn);         // Connect TC4 to Nested Vector Interrupt Controller (NVIC)
-
-	REG_TC4_INTFLAG |= TC_INTFLAG_MC1 | TC_INTFLAG_MC0 ;        // Clear the interrupt flags
-	REG_TC4_INTENSET = TC_INTENSET_MC1 | TC_INTENSET_MC0 ;     // Enable TC4 Match CC0 and CC1 interrupts
-	// REG_TC4_INTENCLR = TC_INTENCLR_MC1 | TC_INTENCLR_MC0 ;     // Disable TC4 interrupts
-
-	REG_TC4_CTRLA |= TC_CTRLA_PRESCALER_DIV256 |     // Set prescaler to 256,
-	TC_CTRLA_ENABLE;               // Enable TC4
-	while (TC4->COUNT16.STATUS.bit.SYNCBUSY);        // Wait for synchronization
-	
-}
-
-void init_TC5(){ //initialize TC3 timer
-	//Clock is already initialized by code from TC4
-	
-	// Configure TC4 (16 bit counter by default)
-	
-	
-	//REG_TC5_COUNT16_CC0 = 0x7A0;                      // Set the TC4 CC0 register to  decimal 1952, 0.5 sec
-	//while (TC5->COUNT16.STATUS.bit.SYNCBUSY);        // Wait for synchronization
-	REG_TC5_COUNT16_CC1 = 0xF41;                      // Set the TC4 CC1 register to  decimal 3905, 1 sec
-	while (TC5->COUNT16.STATUS.bit.SYNCBUSY);        // Wait for synchronization
-	
-	//Enabling interrupts
-	NVIC_SetPriority(TC5_IRQn, 0);    // Set the Nested Vector Interrupt Controller (NVIC) priority for TC4 to 0 (highest)
-	NVIC_EnableIRQ(TC5_IRQn);         // Connect TC4 to Nested Vector Interrupt Controller (NVIC)
-
-	REG_TC5_INTFLAG |= TC_INTFLAG_MC1 | TC_INTFLAG_MC0 ;        // Clear the interrupt flags
-	REG_TC5_INTENSET = TC_INTENSET_MC1 | TC_INTENSET_MC0 ;     // Enable TC4 Match CC0 and CC1 interrupts
-	// REG_TC5_INTENCLR = TC_INTENCLR_MC1 | TC_INTENCLR_MC0 ;     // Disable TC4 interrupts
-
-	REG_TC5_CTRLA |= TC_CTRLA_PRESCALER_DIV256 |     // Set prescaler to 256,
-	TC_CTRLA_ENABLE;               // Enable TC4
-	while (TC5->COUNT16.STATUS.bit.SYNCBUSY);        // Wait for synchronization
-	
-}
-
-void TC4_Handler()                              // Interrupt Service Routine (ISR) for timer TC4
-{
-	// Check for match counter 0 (MC0) interrupt
-	if (TC4->COUNT16.INTFLAG.bit.MC0 && TC4->COUNT16.INTENSET.bit.MC0)
+void TCC2_Handler(void){
+	//CCO 1 sec counter
+	if ( hri_tcc_get_interrupt_MC0_bit(TCC2) )
 	{
-		// compare 0 (CC0) code here:
+		//1. Increase test time by 1s
+		test_time_count++;
 		
-		REG_TC4_INTFLAG = TC_INTFLAG_MC0;         // Clear the MC0 interrupt flag
+		//2. Clear interrupt bit
+		hri_tcc_clear_interrupt_MC0_bit(TCC2);
+		
 	}
-
-	// Check for match counter 1 (MC1) interrupt
-	if (TC4->COUNT16.INTFLAG.bit.MC1 && TC4->COUNT16.INTENSET.bit.MC1)
+	
+	//CC1 ADC rate
+	if ( hri_tcc_get_interrupt_MC1_bit(TCC2) )
 	{
-		//compare 1 (CC1) code here:
-		REG_PORT_OUTTGL1 = LED0; //toggle LED
+		// 1. Read from ADC
 		
-		REG_TC4_INTFLAG = TC_INTFLAG_MC1;        // Clear the MC1 interrupt flag
+		//2. Clear interrupt bit
+		hri_tcc_clear_interrupt_MC1_bit(TCC2);
+		
 	}
 	
 }
 
-void TC5_Handler(){
-	// Check for match counter 1 (MC1) interrupt
-	if (TC4->COUNT16.INTFLAG.bit.MC1 && TC4->COUNT16.INTENSET.bit.MC1)
-	{
-		//compare 1 (CC1) code here:
-		test_time_count++; //increament +1 every second
-		
-		REG_TC4_INTFLAG = TC_INTFLAG_MC1;        // Clear the MC1 interrupt flag
-	}
+void WDT_Handler(void){
+	//early interrupt for WDT, maybe send a message to serial that system is about to reset and resend test configurations
+	//we can discuss on this
+	
 }
 
 /*
@@ -199,7 +181,7 @@ Output: integer
 int twos_complement_to_int (int value, int num_bits)
 {
 	// msb =  value >> (num_bits-1); //collecting MSB
-	if (num_bits <= 2 || num_bits == NULL || value > (pow(2,num_bits)-1)) return NULL; //error checking
+	if (num_bits <= 2 || num_bits == '\0' || value > (pow(2,num_bits)-1)) return '\0'; //error checking
 
 	else if (value >> (num_bits-1)){	//if msb ==1, negative values
 		return (( value & (int)(pow(2,num_bits-1)-1)) - (pow(2,num_bits-1)));
@@ -243,13 +225,13 @@ int magnetic_field_mT(int voltage){
 	#define QUIESCENT_VOLTAGE 1650              //1.65V Low->1.638, mean-> 1.65, high-> 1.662 measure this
 	#define MAGNETIC_SENSITIVITY 1.35           //1.35mv/G --> 13.5mV/mT Low->1.289, mean-> 1.3, high-> 1.411
 	#define TEMPERATURE_SENSITIVITY   0.12      //0.12%/C
-	#define ERROR 1.5							//1.5% ERROR
+	#define magnetic_sesnor_ERROR 1.5							//1.5% ERROR
 	#define VOLTAGE_CLAMP_HIGH 2970			    //2.97V +40 mT
 	#define VOLTAGE_CLAMP_LOW 0.33			    //2.97V -40mT
 	#define MAX_MAGNETIC_FIELD 40              //40mT
 	#define MIN_MAGNETIC_FIELD -40              //-40mT
 	
-	if (voltage == NULL) return NULL;
+	if (voltage == '\0') return '\0';
 	else if (voltage == VOLTAGE_CLAMP_HIGH ) return MAX_MAGNETIC_FIELD ; //we can find some sort of error to throw here
 	else if (voltage == VOLTAGE_CLAMP_LOW) return MIN_MAGNETIC_FIELD ;
 	else return ((voltage-QUIESCENT_VOLTAGE)/MAGNETIC_SENSITIVITY);   //polarity depends on the direction of the field
@@ -270,16 +252,11 @@ void run_system_FSM (int state){
 	
 	#define IDLE 0 //idle state
 	#define ADC_UPDATE 1 //reading data from the ADC using SPI at intervals
-	#define SERIAL_UPDATE 2 //sending data back to the computer through serial at intervals
+	#define SERIAL_UPDATE 2//sending data back to the computer through serial at intervals
 	#define Heater_Update 3 //running the heater_FSM and get the updates
 	#define Magnetic_Field_update 4 //running the magnetic field FSM and get the updates
 	
-	//Inputs from the MCU
-	int desired_temperature = 0;     //in C
-	int desired_magnetic_field = 0;  //in mT
-	int desired_time_for_test = 0;	 //time in minutes
-	int desired_FPGA_voltage =0;     //in mV
-	int serial_output_rate=1000;                 //rate to read data in ms
+	
 	
 	//Magnetic FSM variables
 	int measured_magnetic_field; // in mT
@@ -307,10 +284,12 @@ void run_system_FSM (int state){
 					
 					
 					//2. Setting timer compare value from data rate
-					timer4_compare_value = ((TIMER4_FREQUENCY*serial_output_rate)/(TIMER4_PRESCALAR*1000) )- 1; //sets the timer compare value
+					TCC1_compare_value = ((TCC1_FREQUENCY_VAL*serial_output_rate)/(TCC1_PRESCALAR_VAL*1000) )- 1; //sets the timer compare value
 					
-					REG_TC4_COUNT16_CC1 = timer4_compare_value;                      // Set the TC4 CC1 register, default decimal 3905, 1 sec
-					while (TC4->COUNT16.STATUS.bit.SYNCBUSY);        // Wait for synchronization
+					hri_tcc_write_CC_reg(TCC1, 0, TCC1_compare_value);
+					
+					//REG_TC4_COUNT16_CC1 = TCC1_compare_value;                      // Set the TC4 CC1 register, default decimal 3905, 1 sec
+					//while (TC4->COUNT16.STATUS.bit.SYNCBUSY);        // Wait for synchronization
 				
 				}
 				else if (TEST_STOP){
@@ -348,6 +327,12 @@ void run_system_FSM (int state){
 			break;
 		case Magnetic_Field_update:{
 			//update actual and desired magnetic field, fun magnetic field FSM
+			//1. state_transtions
+			magnetic_fsm_transition();
+			//2.state_run
+			magnetic_fsm_run();
+			
+			
 		}
 			break;
 		default :state = IDLE;
@@ -407,16 +392,16 @@ void ADC_array_convert(void){
 	//24 OFFSET, 25-> ADC VCC in mV,26-> ADC TEMP in C, 27-> GAIN V/V, 28-> REF
 	#define ADC_temp_sensor_coefficient 563 //563uV if ADS1158 and test PCB temperatures are forced together, 
 											//394uV if ADS1158 temperature is forced and test PCB is in free air
-	 for (int i =0, i <= 23, i++)
+	 for (int i =0; i <= 23; i++)
 	 {
-		 converted_ADC_data[i] = ADC_mv(raw_ADC_data[i]); //converting all the voltages to millivolts
+		 converted_ADC_data[i] = ADC_mv(raw_ADC_data[i], ADC_GAIN); //converting all the voltages to millivolts
 	 }
 	//for OFFSET conversion
 	converted_ADC_data[24] = raw_ADC_data[24];
 	//for VCC conversion
 	converted_ADC_data[25] = ((float) raw_ADC_data[25]/(float) 3072);
 	//for ADC temp conversion
-	converted_ADC_data[26] =( ((float) (1000*ADC_mv( raw_ADC_data[26]) - 168000)/(float) ADC_temp_sensor_coefficient)+ 25);
+	converted_ADC_data[26] =( ((float) (1000*ADC_mv( raw_ADC_data[26], ADC_GAIN) - 168000)/(float) ADC_temp_sensor_coefficient)+ 25);
 	//ADC GAIN conversion
 	converted_ADC_data[27] = ((float) raw_ADC_data[27]/(float) 30720);
 	//ADC REF conversion
@@ -425,3 +410,47 @@ void ADC_array_convert(void){
 return;
 }
 
+/************************************************************************/
+/* MAGNETIC FIELD FSM                                                   */
+/************************************************************************/
+/*	This is a state machine for the Magnetic circuit program--
+	An external function is needed to keep updating the actual and desired Magnetic field values
+*/
+#define magnetic_fsm_idle_state 0
+#define magnetic_fsm_increase_state 1
+#define magnetic_fsm_decrease_state 2
+#define magnetic_error 0.015 //error is 1.5% gotten from the magnetic field data sheet
+
+void magnetic_fsm_transition(void)
+{
+	if (magnetic_fsm_state == magnetic_fsm_idle_state) 
+	{
+		if (measured_magnetic_field < (desired_magnetic_field*(1-magnetic_error)) ) magnetic_fsm_state = magnetic_fsm_increase_state;
+		else if (measured_magnetic_field > (desired_magnetic_field*(1 + magnetic_error)) ) magnetic_fsm_state = magnetic_fsm_decrease_state; //else if equal, state is unchanged
+	}
+	else if (magnetic_fsm_state == magnetic_fsm_increase_state) 
+	{
+		if (measured_magnetic_field >= desired_magnetic_field) magnetic_fsm_state = magnetic_fsm_idle_state; //else state is unchanged
+	}
+	else if (magnetic_fsm_state == magnetic_fsm_decrease_state) 
+	{
+		if (measured_magnetic_field <= desired_magnetic_field) magnetic_fsm_state = magnetic_fsm_idle_state; //else state is unchanged
+	}
+}
+void magnetic_fsm_run(void)
+{
+	if (magnetic_fsm_state == magnetic_fsm_idle_state){	} //no change
+	else if (magnetic_fsm_state == magnetic_fsm_increase_state) 
+	{
+		//PWM is increased until desired state
+		if (magnetic_PWM_duty >= 255) magnetic_PWM_duty=255;
+		else magnetic_PWM_duty++;
+	}
+	else if (magnetic_fsm_state == magnetic_fsm_decrease_state) 
+	{
+		//PWM is decreased until desired state
+		if (magnetic_PWM_duty <= 0) magnetic_PWM_duty=0;
+		else magnetic_PWM_duty--;
+	}
+	//analogWrite(MG_pin, MG_duty); //write to the pin after changing the duty
+}
