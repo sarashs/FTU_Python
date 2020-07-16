e8205a8229325bc5a4bc72b1522449d8f1360599
ACheong97 <32020345+ACheong97@users.noreply.github.com>
2020-07-15 23:00:36 -0700

    Update instruction_list_parser.ino

--- instruction_list_parser.ino
+++ instruction_list_parser.ino
@@ -13,7 +13,21 @@ void setup() {
   
   //char json[] = "[{\"id\": 0,\"description\": \"Apply required stress for 2 hours, etcetera\",\"apply_params\": {\"chip_temp\": 120, \"v_stress\": -0.4},\"meas_params\": {\"chip_temp\": {\"units\": \"seconds\", \"freq\": 1},\"v_th\": {\"units\": \"milliseconds\", \"freq\": 10}},\"next_cond\": {\"units\": \"hours\", \"time\": 2},\"goto_cond\": {\"id\": 2, \"cond\": \"iterations>=10\"}},{\"id\": 1,\"description\": \"Note that the description and goto_cond fields should be optional, and that the next_cond nested dict must contain either cond or units and time, exclusive of each other, and that this format should be validated.\",\"apply_params\": {\"chip_temp\": 0, \"v_stress\": 0},\"meas_params\": {\"chip_temp\": {\"units\": \"seconds\", \"freq\": 1}, \"v_th\": {\"units\": \"seconds\", \"freq\": 1}},\"next_cond\": {\"cond\": \"chip_temp<=25\"},\"goto_cond\": {\"id\": 2, \"cond\": \"iterations>=10\"}}]";
   DynamicJsonDocument input_doc(2048); //we use dynamic allocation as the documention recommends this for 1kb+ sized files
-  Serial.setTimeout(10000);
+
+  while(!Serial.available()){} 
+
+  while(Serial.available()){
+    if(Serial.available()>0){ //if there's any characters, read it
+      received_string = Serial.readString(); 
+    }
+  if(received_string.length() > 0){
+    Serial.print("Arduino received: "); //send received string back to python
+    Serial.println(received_string);
+  }
+  }
+  Serial.flush(); //Waits for the transmission of outgoing serial data to complete
+  
+  //Serial.setTimeout(5000);
   Serial.println("r_string is:");
   Serial.println(received_string);
   
@@ -25,20 +39,21 @@ void setup() {
     Serial.println(err.c_str()); //return string representation of the error
     return;
   }
-    
+
+  Serial.print("parsing success");
   //extract params
   JsonObject first_input_element = input_doc[0];
-//
   int id = first_input_element["id"];
   const char* description = first_input_element["description"];
   int applied_temp = first_input_element["meas_params"]["chip_temp"]["freq"];
   float applied_stress = first_input_element["apply_params"]["v_stress"];
 
-    //use params
+  //use params
   Serial.println(id);
   Serial.println(description);
   Serial.println(applied_temp);
-  //Serial.println(output_doc);
+  Serial.println(applied_stress);
+  
 //  //MCU does the conversions
 //  
 //  //create output nested objects and append them to the array
