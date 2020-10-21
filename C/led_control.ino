637434bc3f1ec73bd76718a0d772105c09e37dbb
Rohit Singh <rkochhar@uwaterloo.ca>
2020-10-21 10:56:34 -0300

    Led Control Revisions
    - C\HeaderFiles\led_control.h
      - Created file
      - Moved Macros from .ino -> .h
    - C\lead_control.ino
      - Moved Macros from .ino -> .h
      - Added function descriptions

--- C/led_control.ino
+++ C/led_control.ino
@@ -1,15 +1,46 @@
-int ledPin = A1;
+/*******************************************************************************
+*	File Name: led_control.ino
+*
+* File Description: This file simply controls the leds
+*
+* History: Created by Valentine
+*******************************************************************************/
+// Includes --------------------------------------------------------------------
+#include "HeaderFiles/led_control.h"
+
+// Global variables ------------------------------------------------------------
 int incomingByte;
 
+// Functions -------------------------------------------------------------------
+/*******************************************************************************
+*	Function Name: setup()
+*
+* Function inputs: None
+*	Function outputs: None
+*
+* Function Description: This function runs once upon execution
+*
+* History: Created by Valentine
+*******************************************************************************/
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600); 
-pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
+/*******************************************************************************
+*	Function Name: loop()
+*
+* Function inputs: None
+*	Function outputs: None
+*
+* Function Description: This function runs repeatedly
+*
+* History: Created by Valentine
+*******************************************************************************/
 void loop() {
   // put your main code here, to run repeatedly:
-   // see if there's incoming serial data:
+  // see if there's incoming serial data:
   if (Serial.available() > 0) {
     // read the oldest byte in the serial buffer:
     incomingByte = Serial.read();
